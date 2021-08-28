d57c44c4bf00bdfa6705328445a24810e59b4cde
Juan Camilo <juancmz98@gmail.com>
2021-08-27 20:26:37 -0500

    Se añade herencia de Account

---
+++ Java/Driver.java
@@ -0,0 +1,7 @@
+package Java;
+
+public class Driver extends Account {
+  public Driver(String username, String document){
+    super(username, document);
+  }
+}
---
+++ Java/User.java
@@ -0,0 +1,7 @@
+package Java;
+
+public class User extends Account{
+  public User(String username, String document){
+    super(username, document);
+  }
+}
